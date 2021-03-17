333793afa04519a4cf06bd0cc1294bc8ca5547da
Hongyu Shi <shihongyu1996@gmail.com>
2021-03-17 13:29:53 -0400

    Add: gene mode

--- __main__.py
+++ __main__.py
@@ -1,6 +1,15 @@
 import argparse
 from src import process_input_matrices, process_output_matrices, run_clonealign_pyro
 
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 def main():
     parser = argparse.ArgumentParser(description='clonealign inputs: expr matrix, gene * clone cnv matrix')
@@ -8,6 +17,9 @@ def main():
     parser.add_argument('-c', '--clone', nargs=1, help='gene * clone cnv matrix')
     parser.add_argument('-a', '--assignment', nargs=1, help='output path for clone assignment')
     parser.add_argument('-s', '--gene_score', nargs=1, help='output path for gene type scores')
+    parser.add_argument('-g', "--gene_mode", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="Activate gene mode.")
     args = parser.parse_args()
 
     expr, cnv, expr_csv, cnv_csv = process_input_matrices(args.expr[0], args.clone[0])
