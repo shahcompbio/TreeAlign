c7a69c8f2abf69e5e6472402fec8c351f3ba689f
Hongyu Shi <shih@juno.cbio.private>
2021-06-16 10:11:19 -0400

    Fix: input process bug

--- __main__.py
+++ __main__.py
@@ -22,17 +22,16 @@ def main():
     parser.add_argument('-e', '--expr', nargs=1, help='expr matrix')
     parser.add_argument('-c', '--clone', nargs=1, help='gene * clone cnv matrix')
     parser.add_argument('-a', '--assignment', nargs=1, help='output path for clone assignment')
-    parser.add_argument('-s', '--gene_score', nargs=1, default='', help='output path for gene type scores')
-    parser.add_argument('-f', '--fold_change', nargs=1, default='', help='output path for gene fold change')
+    parser.add_argument('-s', '--gene_score', nargs=1, default='place_holder', help='output path for gene type scores')
+    parser.add_argument('-f', '--fold_change', nargs=1, default='place_holder', help='output path for gene fold change')
     parser.add_argument('-r', '--repeat_times', type=int, default=1, help="number of times of repeating the clonealign run")
     parser.add_argument('-g', '--gene_mode', nargs=1, default='default_model', help='which model to use')
     args = parser.parse_args()
-
+    
     expr, cnv, expr_csv, cnv_csv = process_input_matrices(args.expr[0], args.clone[0])
 
     repeat_times = int(args.repeat_times)
     for i in range(repeat_times):
-        print(args.gene_mode)
         clone_assign_prob, gene_type_score, gene_fold_change = run_clonealign_pyro(cnv, expr, args.gene_mode[0])
         if i == 0:
             print("output the first results!")
